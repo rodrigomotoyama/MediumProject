a38fb3ece15c06759471d9cbadd4d753663ce2ba
Rodrigo Motoyama <rodrigo@rodrigo.motoyama>
2020-04-11 16:43:43 -0300

    Combinei a tabela de estats com a tabela dos playoffs. Arrumei a idade que estava sempre como a ultima idade já registrada

--- OnBasePercentage/obp_script.R
+++ OnBasePercentage/obp_script.R
@@ -63,6 +63,8 @@ for (i in 2010:2019){
   assign(df, mlb_df)
   # print(as.character(i))
 }
+combined_mlb_df
+
 playoff_teams <- read_csv('mlb_playoffs_teams_2010_2019.csv', 
                           col_names = c("year", "score", "matchup"))%>% 
   mutate(series = year %>% str_sub(start = 6),
@@ -75,13 +77,26 @@ playoff_teams <- read_csv('mlb_playoffs_teams_2010_2019.csv',
            str_remove(team2, " \\([:digit:]{2,3}-[:digit:]{2,3}, [AN]L\\)")) %>% 
   select(year, series, winning_team, losing_team) %>% gather(final, team, ends_with('team')) %>% 
   arrange(desc(year)) %>% 
-  mutate(final = final %>% str_sub(end = 1) %>% str_to_upper())
+  mutate(final = final %>% str_sub(end = 1) %>% str_to_upper(), year = as.double(year))
 
 teams_abreviation <- read_csv('currentAbbreviationTeamMLB.csv', col_names = c('team')) %>% 
   separate(team, sep = " - ", into = c("name", "abb")) %>% 
   mutate(abb = abb %>% str_sub(start = -3))
 
-playoff_teams_abb <- playoff_teams %>% left_join(teams_abreviation, by = c('team' = 'name'))
+combined_mlb_df <- playoff_teams %>% left_join(teams_abreviation, by = c('team' = 'name')) %>% 
+  right_join(combined_mlb_df, by = c('abb' = 'team', 'year' = 'year')) %>% 
+  mutate(series = if_else(series %>% is.na(), "Eliminated", series),
+         final = if_else(final %>% is.na(), "-", final)) %>% select(!team) %>% 
+  group_by(player) %>%  mutate(age = age - (max(year) - year))
+
+playoff_teams_abb %>% group_by(player) %>%  mutate(age = age - (max(year) - year)) %>% 
+  filter(player == 'Ichiro Suzuki')
+
+
+
+
+
+
 combined_mlb_df %>% arrange(player)
 
 mlb_df_300_ab <- combined_mlb_df %>% 
